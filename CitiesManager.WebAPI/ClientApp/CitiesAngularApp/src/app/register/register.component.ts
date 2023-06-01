40d1fc1f63cf54fe9b46a22ae23ba23495f7ad73
srkans <serkansacma@hotmail.com>
2023-06-01 16:23:08 +0300

    register user component

---
+++ CitiesManager.WebAPI/ClientApp/CitiesAngularApp/src/app/register/register.component.ts
@@ -0,0 +1,62 @@
+import { Component } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+import { RegisterUser } from '../models/register-user';
+import { AccountService } from '../services/account.service';
+
+@Component({
+  selector: 'app-register',
+  templateUrl: './register.component.html',
+  styleUrls: ['./register.component.css']
+})
+export class RegisterComponent {
+  registerForm: FormGroup;
+  isRegisterFormSubmitted: boolean = false;
+
+  constructor(private accountService: AccountService, private router: Router) {
+    this.registerForm = new FormGroup({
+      personName: new FormControl(null, [Validators.required]),
+      email: new FormControl(null, [Validators.required]),
+      phoneNumber: new FormControl(null, [Validators.required]),
+      password: new FormControl(null, [Validators.required]),
+      confirmPassword: new FormControl(null, [Validators.required])
+    });
+  }
+
+  get register_personNameControl(): any {
+    return this.registerForm.controls["personName"];
+  }
+  get register_emailControl(): any {
+    return this.registerForm.controls["email"];
+  }
+  get register_phoneNumberControl(): any {
+    return this.registerForm.controls["phoneNumber"];
+  }
+  get register_passwordControl(): any {
+    return this.registerForm.controls["password"];
+  }
+  get register_confirmPasswordControl(): any {
+    return this.registerForm.controls["confirmPassword"];
+  }
+
+  registerSubmitted() {
+    this.isRegisterFormSubmitted = true;
+
+    this.accountService.postRegister(this.registerForm.value).subscribe({
+      next: (response: RegisterUser) => {
+        console.log(response);
+
+        this.isRegisterFormSubmitted = false;
+
+        this.router.navigate(['/cities']);
+
+        this.registerForm.reset();
+      },
+      error: (error: any) => {
+        console.log(error);
+      },
+      complete: () => { }
+    });
+  }
+
+}
