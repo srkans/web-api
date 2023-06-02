41ff4713caeb8364a575ee34ff87a5b9dd78e0d5
srkans <serkansacma@hotmail.com>
2023-06-02 09:18:51 +0300

    login endpoint

--- CitiesManager.WebAPI/Controllers/v1/AccountController.cs
+++ CitiesManager.WebAPI/Controllers/v1/AccountController.cs
@@ -7,6 +7,9 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace CitiesManager.WebAPI.Controllers.v1
 {
+    /// <summary>
+    /// Account Controller
+    /// </summary>
     [AllowAnonymous]
     [ApiVersion("1.0")]
     public class AccountController : CustomControllerBase
@@ -15,6 +18,12 @@ namespace CitiesManager.WebAPI.Controllers.v1
         private readonly SignInManager<ApplicationUser> _signInManager;
         private readonly RoleManager<ApplicationRole> _roleManager;
 
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="userManager"></param>
+        /// <param name="signInManager"></param>
+        /// <param name="roleManager"></param>
         public AccountController(UserManager<ApplicationUser> userManager, SignInManager<ApplicationUser> signInManager, RoleManager<ApplicationRole> roleManager)
         {
             _userManager = userManager;
@@ -22,11 +31,16 @@ namespace CitiesManager.WebAPI.Controllers.v1
             _roleManager = roleManager;
         }
 
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="registerDTO"></param>
+        /// <returns></returns>
         [HttpPost("register")]
         public async Task<ActionResult<ApplicationUser>> PostRegister(RegisterDTO registerDTO)
         {
             //Validation
-            if(ModelState.IsValid == false)
+            if (ModelState.IsValid == false)
             {
                 string errorMessage = string.Join(" | ", ModelState.Values.SelectMany(v => v.Errors).Select(e => e.ErrorMessage));
 
@@ -44,7 +58,7 @@ namespace CitiesManager.WebAPI.Controllers.v1
 
             IdentityResult result = await _userManager.CreateAsync(user, registerDTO.Password);
 
-            if(result.Succeeded)
+            if (result.Succeeded)
             {
                 //sign-in
                 _signInManager.SignInAsync(user, isPersistent: false);
@@ -58,13 +72,17 @@ namespace CitiesManager.WebAPI.Controllers.v1
                 return Problem(errorMessage);
             }
         }
-
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="email"></param>
+        /// <returns></returns>
         [HttpGet]
         public async Task<IActionResult> IsEmailAlreadyRegistered(string email)
         {
             ApplicationUser? user = await _userManager.FindByEmailAsync(email);
 
-            if(user == null)
+            if (user == null)
             {
                 return Ok(true);
             }
@@ -72,6 +90,53 @@ namespace CitiesManager.WebAPI.Controllers.v1
             {
                 return Ok(false);
             }
+        }
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="loginDTO"></param>
+        /// <returns></returns>
+        [HttpPost("login")]
+        public async Task<IActionResult> PostLogin(LoginDTO loginDTO)
+        {
+            //Validation
+            if (ModelState.IsValid == false)
+            {
+                string errorMessage = string.Join(" | ", ModelState.Values.SelectMany(v => v.Errors).Select(e => e.ErrorMessage));
+
+                return Problem(errorMessage);
+            }
+
+            var result = await _signInManager.PasswordSignInAsync(loginDTO.Email, loginDTO.Password, isPersistent: false, lockoutOnFailure: false);
+
+            if (result.Succeeded)
+            {
+                ApplicationUser? user = await _userManager.FindByEmailAsync(loginDTO.Email);
+
+                if (user == null)
+                {
+                    return NoContent();
+                }
+                else
+                {
+                    return Ok(new { personName = user.PersonName, email = user.Email });
+                }
+            }
+            else
+            {
+                return Problem("Invalid email or password");
+            }
+        }
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <returns></returns>
+        [HttpGet("logout")]
+        public async Task<IActionResult> GetLogout()
+        {
+            await _signInManager.SignOutAsync();
+
+            return NoContent();
         }
     }
 }
